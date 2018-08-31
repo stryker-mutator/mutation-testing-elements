aaa3084d5667d9c78f87ae8124daedc1b57b41ce
legopiraat <3492598+legopiraat@users.noreply.github.com>
2018-08-31 19:21:50 +0200

    chore(schema): Initial version of mutation schema
    * Initial version of the json report structure schema
    
    * Remove todo's and line numbers until we agree on which we want to use
    
    * Add required fields and retrictions for some fields
    
    * Add basic test to see validate json based on the schema
    
    * Refactor(test): Add some semicolons.
    
    * change version to 0.0.1
    
    * alter package.json. Change license
    
    * Allow an empty list of childResults.
    
    * Allow empty array for mutants
    
    * Remove defaults from scheme

---
+++ test/JsonValidatorTest.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var Ajv = require('ajv');
+var schema = require('../mutation-testing-report-schema.json');
+
+describe('JsonSchema', function () {
+    var ajv = new Ajv().addSchema(schema, 'mutation-testing-report-schema');
+
+    it('should return true when the given json file is valid based on the json schema', function () {
+        var data = require('./resources/valid-mutation-testing-report.json');
+        var valid = ajv.validate('mutation-testing-report-schema', data);
+
+        expect(valid).to.be.true;
+    });
+
+    it("should return false when the given json file is invalid based on the json schema", function() {
+        var data = require('./resources/invalid-mutation-testing-report.json');
+        var valid = ajv.validate('mutation-testing-report-schema', data);
+
+        expect(ajv.errorsText(ajv.errors)).to.contain("data should have required property 'health'");
+        expect(valid).to.be.false;
+    });
+});
