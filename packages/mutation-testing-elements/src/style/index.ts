7d585f378ac2bd057a81e8d63e118845ed86e19b
Nico Jansen <nico.jansen@infosupport.com>
2019-03-12 11:59:04 +0100

    Choose highlightjs "github" style code highlighting

--- packages/mutation-testing-elements/src/style/index.ts
+++ packages/mutation-testing-elements/src/style/index.ts
@@ -1,6 +1,6 @@
-import { css, CSSResult, unsafeCSS } from 'lit-element';
+import { unsafeCSS } from 'lit-element';
 const bootstrapCss = require('./bootstrap.scss');
 const highlightjsCss = require('./highlightjs.scss');
 
-export const bootstrap: CSSResult = css`${unsafeCSS(bootstrapCss.toString())}`;
-export const highlightJS: CSSResult = css`${unsafeCSS(highlightjsCss.toString())}`;
+export const bootstrap = unsafeCSS(bootstrapCss.toString());
+export const highlightJS = unsafeCSS(highlightjsCss.toString());
