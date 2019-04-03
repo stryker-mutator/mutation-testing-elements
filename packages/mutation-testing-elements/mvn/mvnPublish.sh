611534f6146611bcac9da75d16bd9c93ee29d0f6
Hugo van Rijswijk <hugo.vanrijswijk@infosupport.com>
2019-04-03 18:06:27 +0200

    Add Sonatype release script

---
+++ packages/mutation-testing-elements/mvn/mvnPublish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+PACKAGE_VERSION=$(cat ../package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+echo "Deploying $PACKAGE_VERSION..."
+
+echo "Importing gpg key..."
+echo "$PGP_SECRET" | base64 --decode | gpg --import
+
+echo "Starting deploy process"
+mvn -P release versions:set "-DnewVersion=$PACKAGE_VERSION"
+mvn -P release install --settings settings.xml
---
+++ publish.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+set -e
+
+# npm run lerna:publish
+
+cd packages/mutation-testing-elements/mvn && ./mvnPublish.sh
