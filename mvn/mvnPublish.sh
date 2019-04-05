d216b554208b44684f26a9d6a6988cb327b944de
Hugo van Rijswijk <hugo.vanrijswijk@infosupport.com>
2019-04-05 15:59:13 +0200

    Use npm/lerna postpublish steps for maven deploy

--- mvn/mvnPublish.sh
+++ mvn/mvnPublish.sh
@@ -1,35 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Importing gpg key..."
-echo "$PGP_SECRET" | base64 --decode | gpg --import
+# Called from packages/$project by npm postpublish, so the pwd is already the project folder
 
-function deploy() {
-  ORIG_DIR=$PWD
-  PROJ_DIR=$1
+PROJ_BASE=$(basename `pwd`)
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[", ]//g')
 
-  cd $PROJ_DIR
+echo "Deploying $PROJ_BASE $PACKAGE_VERSION..."
 
-  PROJ_BASE=$(basename `pwd`)
+echo "Copying dist files to resources"
+mkdir -p mvn/resources
+cp -r dist mvn/resources/$PROJ_BASE
 
-  mkdir -p mvn/resources
-  cp -r dist mvn/resources/$PROJ_BASE
-
-  PACKAGE_VERSION=$(cat package.json \
-    | grep version \
-    | head -1 \
-    | awk -F: '{ print $2 }' \
-    | sed 's/[", ]//g')
-
-  echo "Deploying $PROJ_BASE $PACKAGE_VERSION..."
-
-  echo "Starting deploy process"
-  cd mvn
-  mvn -P release versions:set "-DnewVersion=$PACKAGE_VERSION"
-  mvn -P release deploy --settings $ORIG_DIR/mvn/settings.xml
-
-  cd $ORIG_DIR
-}
-
-deploy packages/mutation-testing-elements
-deploy packages/mutation-testing-report-schema
+echo "Starting deploy process"
+cd mvn
+mvn -P release versions:set "-DnewVersion=$PACKAGE_VERSION"
+mvn -P release deploy --settings ../../../mvn/settings.xml
