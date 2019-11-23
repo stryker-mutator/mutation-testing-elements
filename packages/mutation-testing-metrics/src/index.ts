3017700a5fb47a73c91e3fd9c9b849fbb49d3520
Nico Jansen <nico.jansen@infosupport.com>
2019-11-24 00:59:41 +0100

    feat(metrics): make `normalizeFileNames` public

--- packages/mutation-testing-metrics/src/index.ts
+++ packages/mutation-testing-metrics/src/index.ts
@@ -1,9 +1,11 @@
 import { MetricsResult } from './api/MetricsResult';
 import { Metrics } from './api/Metrics';
 import { calculateMetrics } from './calculateMetrics';
+import { normalizeFileNames } from './helpers';
 
 export {
   MetricsResult,
   Metrics,
-  calculateMetrics
+  calculateMetrics,
+  normalizeFileNames
 };
