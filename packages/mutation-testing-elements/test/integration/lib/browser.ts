be14d243140f708455dcb45f2402d7df86c56995
Nico Jansen <nico.jansen@infosupport.com>
2019-12-11 12:57:58 +0100

    Remove unused code in browser.ts

--- packages/mutation-testing-elements/test/integration/lib/browser.ts
+++ packages/mutation-testing-elements/test/integration/lib/browser.ts
@@ -5,13 +5,6 @@ let browser: WebDriver | null = null;
 
 export async function init() {
   const baseOptions = new chrome.Options();
-  // Use headless chrome on the build server
-  if (process.env.GITHUB_ACTIONS) {
-    process.env.CHROME_DRIVER = process.env.ChromeWebDriver;
-    console.log('Detecting Github actions, using chrome driver at');
-  }
-  console.log(`Using chrome driver at "${process.env.CHROME_DRIVER}"`);
-
   const chromeOptions = process.env.TRAVIS || process.env.GITHUB_ACTIONS ?
     baseOptions.headless() :
     baseOptions;
