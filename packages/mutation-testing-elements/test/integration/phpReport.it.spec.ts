0cc5af48a4ca9c0533d19ccb7a3f27e35b8718b5
Nico Jansen <jansennico@gmail.com>
2020-03-28 22:22:10 +0100

    feat(elements): support syntax highlighting for php (#394)
    Disable automatic loading of syntax highlight settings for unknown languages.
    Include PHP in the supported languages.
    
    Workaround for #393

---
+++ packages/mutation-testing-elements/test/integration/phpReport.it.spec.ts
@@ -0,0 +1,24 @@
+import { ReportPage } from './po/ReportPage';
+import { getCurrent } from './lib/browser';
+import { expect } from 'chai';
+
+describe.only('File report "infection-php-example/TextFileLogger.php"', () => {
+
+  let page: ReportPage;
+
+  before(async () => {
+    page = new ReportPage(getCurrent());
+    await page.navigateTo('');
+    await page.navigateTo('infection-php-example/#TextFileLogger.php');
+  });
+
+  it('should highlight the code', async() => {
+    expect(await page.isCodeHighlighted()).true;
+  });
+
+  it('should show mutants', async() => {
+    // Wait for code to be highted first
+    expect(await page.isCodeHighlighted()).true;
+    expect(await page.mutant('ebf143eb565188ddd7959bfbe70f631f').isButtonVisible()).true;
+  });
+});
