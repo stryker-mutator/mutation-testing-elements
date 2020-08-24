38dd85ec4548288850b5c5e5dff4b82d8b89cb38
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2020-08-24 16:00:49 +0200

    feat(metrics-scala): add optional description field

--- packages/mutation-testing-metrics-scala/circe/src/test/scala/mutationtesting/EncoderTest.scala
+++ packages/mutation-testing-metrics-scala/circe/src/test/scala/mutationtesting/EncoderTest.scala
@@ -29,7 +29,7 @@ class EncoderTest extends munit.FunSuite {
     val result = sut.asJson.noSpaces
 
     val expectedJson =
-      """{"$schema":"https://raw.githubusercontent.com/stryker-mutator/mutation-testing-elements/master/packages/mutation-testing-report-schema/src/mutation-testing-report-schema.json","schemaVersion":"1","thresholds":{"high":80,"low":10},"files":{"src/stryker4s/Stryker4s.scala":{"source":"case class Stryker4s(foo: String)","mutants":[{"id":"1","mutatorName":"BinaryOperator","replacement":"-","location":{"start":{"line":1,"column":2},"end":{"line":2,"column":3}},"status":"Killed"}],"language":"scala"}}}"""
+      """{"$schema":"https://git.io/mutation-testing-report-schema","schemaVersion":"1","thresholds":{"high":80,"low":10},"files":{"src/stryker4s/Stryker4s.scala":{"source":"case class Stryker4s(foo: String)","mutants":[{"id":"1","mutatorName":"BinaryOperator","replacement":"-","location":{"start":{"line":1,"column":2},"end":{"line":2,"column":3}},"status":"Killed"}],"language":"scala"}}}"""
     assertEquals(result, expectedJson)
   }
 }
