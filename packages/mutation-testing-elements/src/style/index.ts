783d72e1c38fa83a0f15aa1acc86a7b207a19e77
Richard Werkman <Richard.Werkman@infosupport.com>
2020-10-23 19:24:05 +0200

    prism dark mode

--- packages/mutation-testing-elements/src/style/index.ts
+++ packages/mutation-testing-elements/src/style/index.ts
@@ -1,8 +1,6 @@
 import { unsafeCSS } from 'lit-element';
 import bootstrapCss from './bootstrap.scss';
-import prismjsCssLight from './prismjs.light.scss';
-import prismjsCssDark from './prismjs.dark.scss';
+import prismjsCss from './prismjs.scss';
 
 export const bootstrap = unsafeCSS(bootstrapCss);
-export const prismjslight = unsafeCSS(prismjsCssLight);
-export const prismjsdark = unsafeCSS(prismjsCssDark);
+export const prismjs = unsafeCSS(prismjsCss);
