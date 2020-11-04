c06673391ee3c58ddfc2d074d3653755ef641c0a
Nico Jansen <jansennico@gmail.com>
2020-11-04 09:46:11 +0100

    fix(ts): make project root optional in TS api (#721)

--- packages/mutation-testing-report-schema/src/api/MutationTestResult.ts
+++ packages/mutation-testing-report-schema/src/api/MutationTestResult.ts
@@ -3,7 +3,7 @@ import { Thresholds } from './Thresholds';
 
 export interface MutationTestResult {
   schemaVersion: string;
-  projectRoot: string;
+  projectRoot?: string;
   thresholds: Thresholds;
   files: FileResultDictionary;
 }
