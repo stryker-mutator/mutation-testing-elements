b2ed54be522c61f65a68520b3eaff7d463185752
Hugo van Rijswijk <git@hugovr.nl>
2021-11-05 12:37:41 +0100

    build(deps-dev): update eslint

--- tasks/schema2ts.js
+++ tasks/schema2ts.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-var-requires */
 // See https://github.com/bcherny/json-schema-to-typescript/issues/358
 
