a2e4c0d895d98710bc0bd1d856291d1c318ab38a
Xander Vedder <xander.vedd@gmail.com>
2024-11-20 21:42:53 +0100

    fix: minor things

--- packages/elements/src/components/file-picker/file-picker.component.ts
+++ packages/elements/src/components/file-picker/file-picker.component.ts
@@ -1,4 +1,4 @@
-import { html, LitElement, nothing, svg, type PropertyValues } from 'lit';
+import { html, LitElement, nothing, type PropertyValues } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { repeat } from 'lit/directives/repeat.js';
 import { when } from 'lit/directives/when.js';
@@ -6,7 +6,7 @@ import { when } from 'lit/directives/when.js';
 import { TestFileModel } from 'mutation-testing-metrics';
 import type { FileUnderTestModel, Metrics, MetricsResult, MutationTestMetricsResult, TestMetrics } from 'mutation-testing-metrics';
 
-import { searchIcon } from '../../lib/svg-icons.js';
+import { mutantFileIcon, searchIcon, testFileIcon } from '../../lib/svg-icons.js';
 import { renderIf, toAbsoluteUrl } from '../../lib/html-helpers.js';
 import { tailwind } from '../../style/index.js';
 import { View } from '../../lib/router.js';
@@ -132,21 +132,11 @@ export class MutationTestReportFilePickerComponent extends LitElement {
   }
 
   #renderTestOrMutantIndication(view: View) {
-    return html`
-      ${when(
-        view === View.mutant,
-        () => svg`
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
-            <path fill-rule="evenodd" d="M2 4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4Zm4.78 1.97a.75.75 0 0 1 0 1.06L5.81 8l.97.97a.75.75 0 1 1-1.06 1.06l-1.5-1.5a.75.75 0 0 1 0-1.06l1.5-1.5a.75.75 0 0 1 1.06 0Zm2.44 1.06a.75.75 0 0 1 1.06-1.06l1.5 1.5a.75.75 0 0 1 0 1.06l-1.5 1.5a.75.75 0 1 1-1.06-1.06l.97-.97-.97-.97Z" clip-rule="evenodd" />
-          </svg>
-        `,
-        () => svg`
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
-            <path fill-rule="evenodd" d="M2 4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4Zm2.22 1.97a.75.75 0 0 0 0 1.06l.97.97-.97.97a.75.75 0 1 0 1.06 1.06l1.5-1.5a.75.75 0 0 0 0-1.06l-1.5-1.5a.75.75 0 0 0-1.06 0ZM8.75 8.5a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 0-1.5h-2.5Z" clip-rule="evenodd" />
-          </svg>
-        `,
-      )}
-    `;
+    return html`${when(
+      view === View.mutant,
+      () => mutantFileIcon,
+      () => testFileIcon,
+    )}`;
   }
 
   #handleFocus() {
@@ -216,10 +206,7 @@ export class MutationTestReportFilePickerComponent extends LitElement {
 
   #scrollActiveLinkInView() {
     const activeLink = this.shadowRoot?.querySelector('a[data-active]');
-
-    if (activeLink != null) {
-      activeLink.scrollIntoView({ block: 'nearest' });
-    }
+    activeLink?.scrollIntoView({ block: 'nearest' });
   }
 
   #handleArrowDown() {
