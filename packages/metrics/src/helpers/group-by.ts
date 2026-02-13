e7a089a81b6b461481dcf454115053a3de32f74f
Andreas Hoornstra <69248785+Andreas02-dev@users.noreply.github.com>
2026-02-13 13:27:46 +0100

    fix(metrics): fix groupBy function (#4060)
    * fix(metrics): fix groupBy function
    
    the groupBy function uses an object as accumulator, which inherits special
    properties like `__proto__`. This causes the nullish coalescing assignment
    operator to skip creating an array for these grouping keys, causing the error
    `TypeError: acc[key].push is not a function`.
    
    closes: #4059

--- packages/metrics/src/helpers/group-by.ts
+++ packages/metrics/src/helpers/group-by.ts
@@ -1,8 +1,18 @@
 export function groupBy<K extends PropertyKey, T>(arr: T[], criteria: (element: T) => K): Partial<Record<K, T[]>> {
-  return arr.reduce((acc: Partial<Record<K, T[]>>, item) => {
-    const key = criteria(item);
-    acc[key] ??= [];
-    acc[key].push(item);
-    return acc;
-  }, {});
+  // @ts-expect-error -- groupBy exists in Node 22+
+  if (Object.groupBy) {
+    // @ts-expect-error -- groupBy exists in Node 22+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+    return Object.groupBy(arr, criteria);
+  }
+
+  return arr.reduce(
+    (acc: Partial<Record<K, T[]>>, item) => {
+      const key = criteria(item);
+      acc[key] ??= [];
+      acc[key].push(item);
+      return acc;
+    },
+    Object.create(null) as Partial<Record<K, T[]>>,
+  );
 }
