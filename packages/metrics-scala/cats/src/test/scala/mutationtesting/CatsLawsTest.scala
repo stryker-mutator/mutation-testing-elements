9376bc7f40a8d315b34d6a8cdefd497d942022cd
Hugo van Rijswijk <git@hugovr.nl>
2026-06-05 21:46:16 +0200

    feat(metrics-scala): add Monoid instances for Position and Location (#4201)

--- packages/metrics-scala/cats/src/test/scala/mutationtesting/CatsLawsTest.scala
+++ packages/metrics-scala/cats/src/test/scala/mutationtesting/CatsLawsTest.scala
@@ -1,16 +1,19 @@
 package mutationtesting
 
-import _root_.cats.kernel.laws.discipline.OrderTests
+import _root_.cats.kernel.laws.discipline.{CommutativeMonoidTests, OrderTests}
 import munit.DisciplineSuite
 import mutationtesting.arbitraries.*
 import mutationtesting.cats.*
 
 class CatsLawsTest extends DisciplineSuite {
 
-  checkAll("Location.OrderTests", OrderTests[Location].order)
-
   checkAll("Position.OrderTests", OrderTests[Position].order)
+  checkAll("Position.MonoidTests", CommutativeMonoidTests[Position].commutativeMonoid)
+
+  checkAll("Location.OrderTests", OrderTests[Location].order)
+  checkAll("Location.MonoidTests", CommutativeMonoidTests[Location].commutativeMonoid)
 
   checkAll("OpenEndLocation.OrderTests", OrderTests[OpenEndLocation].order)
+  checkAll("OpenEndLocation.MonoidTests", CommutativeMonoidTests[OpenEndLocation].commutativeMonoid)
 
 }
